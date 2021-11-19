1e7052a7a0444a3b616041123209bb782812154f
Yohann <yohan_raza@yahoo.fr>
2021-11-19 05:39:28 +0100

    fist commit

---
+++ front/js/product.js
@@ -0,0 +1,89 @@
+let currentUrl = window.location.href;
+let url = new URL(currentUrl);
+let productId = url.searchParams.get("id");
+
+let apiUrl = ("http://localhost:3000/api/products/");
+
+let fetchUrl = apiUrl + productId;
+
+
+
+fetch (fetchUrl)
+.then(function(res) {
+  if (res.ok) {
+    return res.json();
+  }
+})
+.then(function(value) {
+  console.log(value);
+  getProduits(value);
+})
+.catch(function(err) {
+  // Une erreur est survenue
+});
+
+class Kanap {
+  constructor(_id, name, price, description, imageUrl, colors, altTxt){
+    this._id = _id;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this.colors = colors;
+    this.altTxt = altTxt;
+  }
+}
+
+function getProduits(value) {
+  produits.push (new Kanap(value._id, value.name, value.price, value.description, value.imageUrl, value.colors, value.altTxt))
+  displayProduits();
+}
+
+
+let produits = []
+
+let title = document.getElementById("title")
+let itemImg = document.getElementsByClassName("item__img")[0]
+let productDescription = document.getElementById("description")
+let productColor = document.getElementById("colors")
+let productPrice = document.getElementById("price")
+
+function displayProduits() {
+  
+  for (let produit of produits) {
+    title.innerHTML += `
+    ${produit.name}`
+    
+    productDescription.innerHTML += `${produit.description}`
+    productPrice.innerHTML += `${produit.price}`
+    itemImg.innerHTML += `<img src="${produit.imageUrl}" alt="${produit.altTxt}">`
+    
+    for ( let color of produit.colors) {
+    productColor.innerHTML += `<option value="${color}">${color}</option>`
+    }
+    
+  };  
+}
+
+
+class cartproduct {
+  constructor(id, quantity, color){
+  this.id = id;
+  this.quantity = quantity;
+  this.color = color;
+}
+}
+
+
+
+
+addToCart.onclick = () => {
+  let  productInCart = []
+  if (localStorage.getItem("cart") !== null) {
+    productInCart = JSON.parse(localStorage.getItem("cart"))
+  }
+
+  productInCart.push(new cartproduct (productId, quantity.value, colors.value))
+
+  localStorage.setItem ("cart", JSON.stringify(productInCart))
+}
---
+++ front/js/script.js
@@ -0,0 +1,58 @@
+fetch ("http://localhost:3000/api/products")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+    getProduits(value);
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
+  class Kanap {
+    constructor(_id, name, price, description, imageUrl, colors, altTxt){
+    this._id = _id;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this.colors = colors;
+    this.altTxt = altTxt;
+    }
+    }
+
+function getProduits(value) {
+    for(let produit of value) {
+      produits.push (new Kanap(produit._id, produit.name, produit.price, produit.description, produit.imageUrl, produit.colors, produit.altTxt))
+    }
+    displayProduits();
+}
+
+
+let produits = []
+
+
+cible = document.getElementById("items")
+
+let urlProduit = new URL('C:/Users/Yohann/Desktop/P5/P5-Dev-Web-Kanap-master/front/html/product.html')
+
+function displayProduits() {
+  
+  for (let produit of produits) {
+    urlProduit.searchParams.set('id',produit._id)
+    cible.innerHTML += `
+
+    <a href=${urlProduit}>
+    <article>
+    <img src=${produit.imageUrl} alt=${produit.altTxt}>
+    <h3 class="productName">${produit.name}</h3>
+    <p class="productDescription">${produit.description}</p>
+    </article>
+    </a>`
+  }
+  
+}
+ 
+
